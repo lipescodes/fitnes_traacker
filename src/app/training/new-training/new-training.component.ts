4f60a4a72f516abffc867ca6bf90d2e04ff7fefd
Bálint Lehel <lehel.balint@pctrade.hu>
2020-06-26 07:35:49 +0200

    intit 2

---
+++ src/app/training/new-training/new-training.component.ts
@@ -0,0 +1,23 @@
+import { Exercise } from './../exercise.model';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { TrainingService } from '../training.service';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-new-training',
+  templateUrl: './new-training.component.html',
+  styleUrls: ['./new-training.component.css']
+})
+export class NewTrainingComponent implements OnInit {
+  @Output() trainingStart = new EventEmitter<void>();
+  exercises: Exercise[] = [];
+  constructor(private trainingService: TrainingService) { }
+
+  ngOnInit(): void {
+    this.exercises = this.trainingService.getAvailableExercises();
+  }
+  onStartTraining(form: NgForm){
+    this.trainingService.startExercise(form.value.exercise);
+  }
+
+}
